e91b035ff7de722ea3a049b66cec6fa645b6beab
Lewis <lewis.greasley@gmail.com>
2024-01-26 22:45:51 +0000

    Added to Vec2

--- include/Vector2.h
+++ include/Vector2.h
@@ -14,18 +14,25 @@ public:
 
     float length() const;
 
+    float dot(const Vector2& vector) const;
+
+    void normalize();
+
+    std::string toString() const;
+
     Vector2& operator+=(const Vector2& vector);
 
     Vector2& operator-=(const Vector2& vector);
 
-    Vector2& operator*=(const Vector2& vector);
+    Vector2& operator*=(float num);
 
-    Vector2& operator/=(const Vector2& vector);
+    Vector2& operator/=(float num);
 
     bool operator==(const Vector2& vector) const;
 
     bool operator!=(const Vector2& vector) const;
 };
+
 inline Vector2::Vector2() : x(0.0), y(0.0) { // same as just x=0 and y=0 in body I think? Just looks neater
 
 }
@@ -33,7 +40,52 @@ inline Vector2::Vector2() : x(0.0), y(0.0) { // same as just x=0 and y=0 in body
 inline Vector2::Vector2(float newX, float newY) : x(newX), y(newY) {
 
 }
-// dot, normalize and operators
+// Returns length or magnitude of vector.
 inline float Vector2::length() const {
     return std::sqrt(x*x + y*y);
+}
+// Dot product of two vectors.
+inline float Vector2::dot(const Vector2& vector) const {
+    return (x*vector.x + y*vector.y);
+}
+// Normalized form.
+inline void Vector2::normalize() {
+    float magnitude = length();
+    x /= magnitude;
+    y /= magnitude;
+}
+// Addition equals operator.
+inline Vector2& Vector2::operator+=(const Vector2& vector) {
+    x += vector.x;
+    y += vector.y;
+    return *this;
+}
+// Subtraction equals operator.
+inline Vector2& Vector2::operator-=(const Vector2& vector) {
+    x -= vector.x;
+    y -= vector.y;
+    return *this;
+}
+// Multiplication equals operator.
+inline Vector2& Vector2::operator*=(float num) {
+    x *= num;
+    y *= num;
+    return *this;
+}
+// Division equals operator.
+inline Vector2& Vector2::operator/=(float num) {
+    x /= num;
+    y /= num;
+    return *this;
+}
+
+inline bool Vector2::operator==(const Vector2& vector) const {
+    return (x == vector.x && y == vector.y);
+}
+
+inline bool Vector2::operator!=(const Vector2& vector) const {
+    return !(*this == vector);
+}
+inline std::string Vector2::toString() const {
+    return "Vector2(" + std::to_string(x) + "," + std::to_string(y) + ")";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <cmath>
 #include "Vector2.h"
 
 
 int main() 
 {
     Vector2 banana(2.0F, 3.0F);
+    Vector2 apple(3.0F, 1.0F);
+    std::cout << apple.toString() << std::endl;
+    apple.normalize();
+    std::cout << apple.toString() << std::endl;
     std::cout << banana.length() << std::endl;
+    std::cout << banana.dot(apple) << std::endl;
 }
 
